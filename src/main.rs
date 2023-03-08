f62b3ee00df1e1125062571d4bcacbbb76fbf7db
Ante Perisic <perisic.ante@gmail.com>
2023-03-08 12:31:39 +0100

    add lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env, fs, process, error::Error};
+use std::{env,process,};
+use minigrep::{Config, run};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,29 +15,4 @@ fn main() {
         println!("Application error:  {}", e);
         process::exit(1);
     }
-}
-
-struct Config {
-    querry: String,
-    file_path: String,
-}
-
-impl Config{
-    fn build (args: Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments !");
-        }
-        
-        let querry =  args[1].clone();
-        let file_path=  args[2].clone();
-        Ok(Config { querry, file_path })
-    }
-}
-
-fn run(c: Config) -> Result<(), Box<dyn Error>>{
-    
-    let contents = fs::read_to_string(c.file_path)?;
-    println!("With text:\n{}", contents);
-
-    Ok(())
 }
