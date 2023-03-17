3934e8af8d73005e0672f9e66d232df9c655745a
Ante Perisic <perisic.ante@gmail.com>
2023-03-17 09:28:19 +0100

    add serach() to run()

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,35 @@ impl Config {
 
 pub fn run(c: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(c.file_path)?;
-    println!("With text:\n{}", contents);
+    //println!("With text:\n{}", contents);
 
     Ok(())
+}
+
+pub fn search<'a>(querry: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut res = Vec::new();
+    
+    for line in contents.lines() {
+        if line.contains(querry){
+            
+            res.push(line.trim());
+        }
+    }
+    res
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result(){
+        let querry = "duct";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.";
+
+         assert_eq!(vec!["safe, fast, productive."], search(querry,contents));
+    }
 }
